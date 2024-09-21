29eca9af7bf32f8bb192206fc80e3cb3f635e7b0
rishabh <rishabh6401@gmail.com>
2024-09-21 11:50:36 +0530

    update

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -3,7 +3,6 @@ const Product = require("../../models/Product");
 const getFilteredProducts = async (req, res) => {
   try {
     const { category = [], brand = [], sortBy = "price-lowtohigh", page = 1, limit = 10 } = req.query;
-
     let filters = {};
 
     if (category.length) {
@@ -52,6 +51,30 @@ const getFilteredProducts = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Some error occurred",
+    });
+  }
+};
+
+const getProductDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "Product not found!",
+      });
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (e) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
     });
   }
 };
